2bfaf40f89860a7e153c63c107b28158c762075c
Giselle <martinezgiselle5@gmail.com>
2023-08-17 14:19:13 -0400

    added home route to server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+
+
+const express = require("express")
+const path = require("path")
+//const routes = require("./routes/index.js")
+const PORT = process.env.PORT || 3001
+const app = express()
+app.use (express.json())
+app.use (express.urlencoded({
+    extended:true
+}))
+//app.use("/api",routes)
+app.use(express.static("public"))
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
+
+app.listen(PORT, function(){
+    console.log(`App is listening on: ${PORT}`)
+})
