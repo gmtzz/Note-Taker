5cb0c61521e957831e287375812e07ae0b2b9ba0
Giselle <martinezgiselle5@gmail.com>
2023-08-17 14:52:45 -0400

    Added posted note route

--- routes/notes.js
+++ routes/notes.js
@@ -9,7 +9,34 @@ const {
 router.get ("/",(req,res)=> res.status(200).json(db))
 
 
-
+router.post("/",(req,res)=>{
+    const{
+        title,
+        text
+    }=req.body
+    if (!title||!text){
+        return res.status(400).json({
+            message:"Unable to save note"
+        })
+    }
+    const postedNote = {
+        title,
+        text,
+        id: uuidv4()
+    }
+    const noteContent= {
+        status:"Success",
+        body: postedNote
+    }
+    db.push(postedNote)
+    fs.writeFileSync("./db/db.json",JSON.stringify(db),(error)=>{
+        if(error){
+            console.error(error)
+        }
+        console.log("Note Posted")
+    })
+    res.json(noteContent)
+})
 
 
 module.exports=router
