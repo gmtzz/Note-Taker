03ff4f0f3abc7ee68ca3545fe7dacdabd6078c5b
Giselle <martinezgiselle5@gmail.com>
2023-08-17 14:30:15 -0400

    made changes to noteshtml

--- server.js
+++ server.js
@@ -11,9 +11,16 @@ app.use (express.urlencoded({
 }))
 //app.use("/api",routes)
 app.use(express.static("public"))
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
 app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
+
+
+
 
 app.listen(PORT, function(){
     console.log(`App is listening on: ${PORT}`)
